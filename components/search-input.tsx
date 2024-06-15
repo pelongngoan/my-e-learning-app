5b3bbf8eced30a91f93497b506d2e35358707199
pelongngoan <long.tl446@gmail.com>
2024-06-15 18:29:09 +0700

    done display publish course card

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,11 +1,11 @@
-import { LucideIcon, Search } from "lucide-react";
+"use client";
+import { Search } from "lucide-react";
 import qs from "query-string";
 
 import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react";
 import { useDebounce } from "@/hooks/use-debounce";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
-import { stringify } from "postcss";
 
 export const SearchInput = () => {
   const [value, setValue] = useState("");
@@ -32,6 +32,8 @@ export const SearchInput = () => {
     <div className="relative">
       <Search className="h-4 w-4 absolute top-3 left-3 text-slate-600" />
       <Input
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
         className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
         placeholder="Search for a course"
       />
